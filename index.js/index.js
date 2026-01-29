7631aa3903d1abbfa6cc2646d54483392fd866e3
Oscar <ov7224@gmail.com>
2026-01-29 10:57:15 -0600

    [created]:creamos la estructura inicial

---
+++ index.js/index.js
@@ -0,0 +1,36 @@
+function cambiar(){
+    const nombre = 'marco'
+    alert('ahora desde archivo ${nombre')
+}
+
+const ecma = () => {
+    alert ('ahora con ecma6',j)
+}
+
+var i = 4
+let j = 10
+
+var i='4'
+//console.log('@@@ variable =>', i ,j)
+
+let isinlove = prompt('estas enamorado?')
+console.log ('@@@ isinlove =>',isinlove, typeof isinlove)
+
+const suma=()=>{
+    let valor1 = prompt ('introduce el primer valor')
+    let valor2 = prompt ('introduce el segundo valor valor')
+    let suma = valor1 + valor2
+    //parseInt() floatInt()
+    const label = document.getElementById('suma')
+    label.textContent= 'el resultado es:$(suma)'
+    if (suma > 100 && suma < 199){
+        console.log('@@@ mayor que 100=>', suma)}
+        else if (suma<30){
+            suma()}
+            else if (suma === 200){
+                console.log('@@@comparando igual a 200 con tipo de variable',suma)
+            }
+        
+    
+    console.log('@@@suma =>',suma)
+}
